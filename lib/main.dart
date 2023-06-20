c51286d84c546720e9b8372ab21195724848cd6a
지혜림 <jee.herim@gmail.com>
2023-06-20 23:57:50 +0900

    Remove dartjs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/widgets.dart';
